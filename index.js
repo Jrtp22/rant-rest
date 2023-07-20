fcf20fa9f5f30f7141925150cffce01adbf9c03d
jrtp22 <jrtp22@gmail.com>
2023-07-19 20:40:54 -0700

    controller added

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+//required modules
 require('dotenv').config()
 const express = require('express')
 const app = express()
-
+//controllers
+app.use('/places', require('./controllers/places'))
+// home page
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
-
+//wildcard
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
-
+// start server
 app.listen(process.env.PORT)
