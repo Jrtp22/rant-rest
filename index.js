624fe9e1a47f82dec92cac08673771ac6ef34345
jrtp22 <jrtp22@gmail.com>
2023-07-24 15:06:59 -0700

    adding home page view

--- index.js
+++ index.js
@@ -2,15 +2,23 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+
+//middleware
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 //controllers
 app.use('/places', require('./controllers/places'))
+
 // home page
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('home')
 })
+
 //wildcard
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
+
 // start server
 app.listen(process.env.PORT)
