930be64ae7175bddfe77d4dc15bcc778ebce6d72
jrtp22 <jrtp22@gmail.com>
2023-07-31 20:53:51 -0700

    added mongo db and places page is showing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 //middleware
 app.set('views', __dirname + '/views')
@@ -11,6 +12,7 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
 //controllers
 app.use('/places', require('./controllers/places'))
@@ -27,3 +29,4 @@ app.get('*', (req, res) => {
 
 // start server
 app.listen(process.env.PORT)
+
--- models/places.js
+++ models/places.js
@@ -1,15 +1,31 @@
-module.exports = [{
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    pic: String,
+    cuisines: { type: String, required: true },
+    city: { type: String, default: 'Anytown' },
+    state: { type: String, default: 'USA' },
+    founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
+
+
+
+/* module.exports = [{
     name: 'H-Thai-ML',
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
     pic: '/images/rest.jpg',
-    id:1
+    id: 1
 }, {
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
     cuisines: 'Coffee, Bakery',
     pic: '/images/coffee-cat.jpg',
-    id:2
-}]
+    id: 2
+}] */
